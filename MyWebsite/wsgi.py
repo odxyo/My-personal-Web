6ae9a551663b64c023dced604a193c0adff7ba79
ayaoVA01 <odxyo54@gmail.com>
2024-05-29 10:38:33 +0700

    3

--- MyWebsite/wsgi.py
+++ MyWebsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web: gunicorn MyWebsite.wsgi')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyWebsite.settings')
 
 application = get_wsgi_application()
